38e229e0e2882eeebc44f79fa86b4dda3bca0f33
na2ki <tpall12341@gmail.com>
2026-05-26 20:38:39 +0900

    modify oled_notify.py

--- oled_notify.py
+++ oled_notify.py
@@ -1,16 +1,12 @@
 import sys
-import time
 from luma.core.interface.serial import i2c
-from luma.core.render import canvas
 from luma.oled.device import ssd1306
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, width=128, height=64)
 
 font_path = "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc"
-
-# 128x64 OLEDなので、大きすぎると入らない
 font = ImageFont.truetype(font_path, 12)
 
 line1 = sys.argv[1] if len(sys.argv) > 1 else ""
@@ -18,15 +14,12 @@ line2 = sys.argv[2] if len(sys.argv) > 2 else ""
 line3 = sys.argv[3] if len(sys.argv) > 3 else ""
 line4 = sys.argv[4] if len(sys.argv) > 4 else ""
 
-print("line1 =", line1)
-print("line2 =", line2)
-print("line3 =", line3)
-print("line4 =", line4)
+image = Image.new("1", (device.width, device.height))
+draw = ImageDraw.Draw(image)
 
-with canvas(device) as draw:
-    draw.text((0, 0), line1, font=font, fill="white")
-    draw.text((0, 16), line2, font=font, fill="white")
-    draw.text((0, 32), line3, font=font, fill="white")
-    draw.text((0, 48), line4, font=font, fill="white")
+draw.text((0, 0), line1, font=font, fill=255)
+draw.text((0, 16), line2, font=font, fill=255)
+draw.text((0, 32), line3, font=font, fill=255)
+draw.text((0, 48), line4, font=font, fill=255)
 
-time.sleep(10)
+device.display(image)
