f275bb314e93525db7847deca6a78033bbd4a59c
na2ki <tpall12341@gmail.com>
2026-05-26 17:49:31 +0900

    modify oled_notify.py

--- oled_notify.py
+++ oled_notify.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
@@ -6,15 +7,20 @@ from luma.oled.device import ssd1306
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, width=128, height=64)
 
-# 引数を受け取る
-# 例: python3 oled_notify.py ONLINE FriendName 15:04
 line1 = sys.argv[1] if len(sys.argv) > 1 else ""
 line2 = sys.argv[2] if len(sys.argv) > 2 else ""
 line3 = sys.argv[3] if len(sys.argv) > 3 else ""
 line4 = sys.argv[4] if len(sys.argv) > 4 else ""
+
+print("line1 =", line1)
+print("line2 =", line2)
+print("line3 =", line3)
+print("line4 =", line4)
 
 with canvas(device) as draw:
     draw.text((0, 0), line1, fill="white")
     draw.text((0, 16), line2, fill="white")
     draw.text((0, 32), line3, fill="white")
     draw.text((0, 48), line4, fill="white")
+
+time.sleep(10)
